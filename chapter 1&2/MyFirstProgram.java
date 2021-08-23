e34dcf3b1c8369f90a47a22c9f58e526eae576b3
AlexanderWang72839 <89221464+AlexanderWang72839@users.noreply.github.com>
2021-08-23 15:03:14 -0500

    Updated MyFirstProgram

--- chapter 1&2/MyFirstProgram.java
+++ chapter 1&2/MyFirstProgram.java
@@ -1,6 +1,6 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        system.out.println("This is a test this is only a test.");
+        System.out.println("This is a test this is only a test.");
         System.out.println("This is a test again!");
     }
 }
