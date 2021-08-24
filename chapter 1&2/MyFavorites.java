64b69b7df7bec79d95831d5d112c1c6a76acd26a
AlexanderWang72839 <89221464+AlexanderWang72839@users.noreply.github.com>
2021-08-24 14:36:01 -0500

    Added Test class(with errors) and MyFavorites class

---
+++ chapter 1&2/MyFavorites.java
@@ -0,0 +1,9 @@
+public class MyFavorites {
+    public static void main(String[] args) {
+    System.out.println("Name: Alex Wang");
+    System.out.println("Brithdate: December 2nd 2006");
+    System.out.println("Favorite hobby: Robotics");
+    System.out.println("Favorite book: Masterminds");
+    System.out.println("Favorite movie: I Don't watch movies");
+    }
+}
---
+++ chapter 1&2/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void man(String[] args) {
+        System.out.bogus(An emergency Brodcast)
+    }
+
+
