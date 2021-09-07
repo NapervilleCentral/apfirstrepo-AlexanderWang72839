550898ed979f67ba4a2eee8fb7d7e76ba09600f2
AlexanderWang72839 <89221464+AlexanderWang72839@users.noreply.github.com>
2021-09-07 14:42:43 -0500

    fixed circleArea

--- chapter 1&2/circleArea.java
+++ chapter 1&2/circleArea.java
@@ -10,7 +10,7 @@ public class circleArea {
     public static void main(String[] args) {
         DecimalFormat format = new DecimalFormat("0.##");
         double Radius = 3; 
-        System.out.println("The area is; " + format.format(Math.sqrt(Radius) * Math.PI));
+        System.out.println("The area is: " + format.format(Math.pow(Radius, 2) * Math.PI));
         System.out.println("The circumference is: " + format.format((Radius * 2) * Math.PI));
     }    
 }
