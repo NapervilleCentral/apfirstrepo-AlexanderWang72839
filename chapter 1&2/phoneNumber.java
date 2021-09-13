ab260d7c68da83aa4029906be9b19c560fded68b
Alex Wang <aloyfusion@gmail.com>
2021-09-13 17:52:04 -0500

    updated PhoneNumber

--- chapter 1&2/phoneNumber.java
+++ chapter 1&2/phoneNumber.java
@@ -5,27 +5,26 @@
  * @version 9/7/21
  */
 import java.util.*;
+import java.text.*;
 
 public class phoneNumber {
     public static void main(String[] args) {
-        //initialize variables and RNG
+        //initialize variables, NumberFormater, and RNG
         Random generator = new Random();
+        DecimalFormat fmt = new DecimalFormat("0000");
         int areaCode1 = generator.nextInt(8);
         int areaCode2 = generator.nextInt(8);
         int areaCode3 = generator.nextInt(8);
-        int exchangeCode1 = generator.nextInt(9);
-        int exchangeCode2 = generator.nextInt(9);
-        int exchangeCode3 = generator.nextInt(9);
-        int subscriberNumber1 = (int)(Math.random() * 10);
-        int subscriberNumber2 = (int)(Math.random() * 10);
-        int subscriberNumber3 = (int)(Math.random() * 10);
-        int subscriberNumber4 = (int)(Math.random() * 10);
-
+        int exchangeCode1 = generator.nextInt(8);
+        int exchangeCode2 = generator.nextInt(5);
+        int exchangeCode3 = generator.nextInt(3);
+        int subscriberNumber = (int)(Math.random() * 10000);
         //print phone number
         System.out.print("Random phone number: " + areaCode1);
         System.out.print(areaCode2);
         System.out.print(areaCode3);
-        System.out.println("-" + exchangeCode1 + "-" + subscriberNumber1);
+        System.out.print("-" + exchangeCode1 + exchangeCode2 + exchangeCode3 + "-");
+        System.out.println(fmt.format(subscriberNumber));
     }
 }
 
